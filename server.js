ed9ca17e31b37f419185d093eb5d3f68176e6124
Dennis <deweis@gmx.ch>
2019-03-09 11:09:11 +0100

    Add basic upload route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -16,6 +18,11 @@ app.get('/', function(req, res) {
 
 app.get('/hello', function(req, res) {
   res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+  console.log('Size: ', req.file.size);
+  res.send('Thank you for uploading');
 });
 
 app.listen(process.env.PORT || 3000, function() {
